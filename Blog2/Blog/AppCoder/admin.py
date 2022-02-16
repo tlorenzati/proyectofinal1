17b55b181540a108c70f8629f7a5fac1cd9acaad
tlorenzati <96602256+tlorenzati@users.noreply.github.com>
2022-02-15 21:51:58 -0300

    Add files via upload

---
+++ Blog2/Blog/AppCoder/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from AppCoder.models import Basquetbolistas, Futbolistas, Rugbiers
+
+
+admin.site.register(Futbolistas)
+admin.site.register(Basquetbolistas)
+admin.site.register(Rugbiers)
+
+
